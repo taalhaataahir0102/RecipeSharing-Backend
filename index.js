be43704bb5422a539d22ec9dba41b6f53a05a274
taalhaataahir0102 <23100293@lums.edu.pk>
2023-06-01 18:28:17 +0500

    latest

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 
 const app = express();
 app.use(cors());
@@ -30,25 +31,84 @@ const userSchema = new mongoose.Schema({
 const User = mongoose.model('User', userSchema);
 
 // Signup endpoint
-app.post('/api/form', (req, res) => {
+app.post('/api/signup', async (req, res) => {
+  try {
     const { fullName, email, password } = req.body;
-  
+
+    // Check if the user already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(409).json({ message: 'User already exists' });
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     // Create a new user
-    const user = new User({ fullName, email, password });
-  
-    // Save the user to the database
-    user
-      .save()
-      .then(() => {
-        res.json({ message: 'Signup successful' });
-      })
-      .catch((err) => {
-        console.error(err);
-        res.status(500).json({ error: 'An error occurred while saving the user' });
-      });
+    const newUser = new User({ fullName, email, password: hashedPassword });
+    await newUser.save();
+
+    return res.status(201).json({ message: 'User created successfully' });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
   });
 
-  app.get('/api/users', (req, res) => {
+
+app.post('/api/signin', async (req, res) => {
+    try {
+      const { email, password } = req.body;
+  
+      // Check if the user exists
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(401).json({ message: 'Invalid credentials' });
+      }
+  
+      // Compare the provided password with the stored hashed password
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+      if (!isPasswordValid) {
+        return res.status(401).json({ message: 'Invalid credentials' });
+      }
+  
+      // Generate JWT token
+      const token = jwt.sign({ userId: user._id }, 'secret-key', {
+        expiresIn: '1h',
+      });
+  
+      return res.status(200).json({ token });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: 'Internal server error' });
+    }
+  });
+  
+  // Protected route
+app.get('/api/protected', authenticateToken, (req, res) => {
+    res.json({ message: 'Protected endpoint accessed' });
+  });
+  
+  // Middleware to authenticate JWT token
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers.authorization;
+    const token = authHeader && authHeader.split(' ')[1];
+  
+    if (token === null) {
+      return res.sendStatus(401);
+    }
+  
+    jwt.verify(token, 'secret-key', (err, user) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+      req.user = user;
+      next();
+    });
+  }
+
+
+app.get('/api/users', (req, res) => {
     User.find()
       .then((users) => {
         res.json(users);
